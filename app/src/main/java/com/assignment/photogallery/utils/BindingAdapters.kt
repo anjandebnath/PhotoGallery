28f34383bb02dd0521dea515f77271a991a6dad6
Anjan Debnath <anjan0151@gmail.com>
2022-06-03 10:52:00 +0600

    feat(placeholder):add place holder image during loading

--- app/src/main/java/com/assignment/photogallery/utils/BindingAdapters.kt
+++ app/src/main/java/com/assignment/photogallery/utils/BindingAdapters.kt
@@ -2,6 +2,7 @@ package com.assignment.photogallery.utils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.assignment.photogallery.R
 import com.bumptech.glide.Glide
 
 
@@ -12,6 +13,12 @@ import com.bumptech.glide.Glide
 fun loadImage(view: ImageView, url: String?) {
     if (!url.isNullOrEmpty()) {
         var imgUrl = url
-        Glide.with(view.context).load(imgUrl).into(view)
+        Glide.with(view.context)
+            .load(imgUrl)
+            .placeholder(R.drawable.loading_animation)
+            .error(R.drawable.ic_broken_image)
+            .into(view)
     }
 }
+
+
