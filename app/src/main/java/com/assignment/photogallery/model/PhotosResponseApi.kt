6b8ed8d8308d7e908718515a480b64ffe85f330d
Anjan Debnath <anjan0151@gmail.com>
2022-06-03 10:17:35 +0600

    feat(coil_lib): data model update and image loading coil library included

--- app/src/main/java/com/assignment/photogallery/model/PhotosResponseApi.kt
+++ app/src/main/java/com/assignment/photogallery/model/PhotosResponseApi.kt
@@ -1,7 +1,3 @@
 package com.assignment.photogallery.model
 
-//data class PhotosResponseApi(
-//    val results: ArrayList<PhotosResponseApiItem>
-//)
-
 class PhotosResponseApi : ArrayList<PhotosResponseApiItem>()
